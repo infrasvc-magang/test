9cda0975f5301f89f8671363f04c2a2b468b10fb
alvaroahmadf <alvaroahmadf@gmail.com>
2024-07-10 14:40:44 +0700

    ubah code jia lagi dan lagi

--- backend/cobaWebPPTX-Jeahan/app.py
+++ backend/cobaWebPPTX-Jeahan/app.py
@@ -6,7 +6,7 @@ from pptx.util import Pt
 from flask import Flask, request, render_template, send_file, jsonify
 
 app = Flask(__name__)
-openai.api_key = "ITU_API_KEY"
+openai.api_key = "INITUH_API_KEY"
 
 @app.route('/')
 def index():
