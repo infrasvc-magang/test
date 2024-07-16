a5450786f48276eb0984fbaee03c6cda89193767
alvaroahmadf <alvaroahmadf@gmail.com>
2024-07-16 15:39:18 +0700

    revisi code search

--- backend/project/app.py
+++ backend/project/app.py
@@ -146,18 +146,5 @@ def search():
 def download_file(filename):
     return send_file(filename, as_attachment=True)
 
-@app.route('/search', methods=['GET'])
-def search():
-    query = request.args.get('query')
-    if query:
-        search_results = PresentationHistory.query.filter(PresentationHistory.title.ilike(f'%{query}%')).order_by(PresentationHistory.created_at.desc()).all()
-    else:
-        search_results = PresentationHistory.query.order_by(PresentationHistory.created_at.desc()).all()
-
-    if request.headers.get('X-Requested-With') == 'XMLHttpRequest':
-        return render_template('history_table.html', history=search_results)
-
-    return render_template('index.html', history=search_results)
-
 if __name__ == '__main__':
     app.run(debug=True)
